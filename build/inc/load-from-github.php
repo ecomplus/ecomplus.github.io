6c9ece49f1ce9e04d78afa8327867ea758ace350
Leonardo Matos <leomp120894@gmail.com>
2018-09-04 10:23:31 -0300

    build reading submodules files (md)

--- build/inc/load-from-github.php
+++ build/inc/load-from-github.php
@@ -1,53 +1,33 @@
 <?php
-function github_api ($url, $full_url = false) {
-  // timeout to prevent rate limit
-  // https://developer.github.com/v3/#rate-limiting
-  sleep(60);
-  if ($full_url) {
-    return get_json($url);
-  } else {
-    return get_json('https://api.github.com' . $url);
+function find_md_files ($repo_dir, $path = '') {
+  // scan directories and find Markdown files
+  $dir = $repo_dir;
+  if ($path !== '') {
+    $dir .= '/' . $path;
+    // add dir level to path before adding filename
+    $path .= '/';
   }
-}
+  $filenames = scandir($dir);
+  $files = [];
 
-function get_repo_docs ($repo, $repo_path = '') {
-  // returns array of Markdown files
-  $files = array();
-
-  // GET content from GitHub API:
-  // https://developer.github.com/v3/repos/contents/
-  $github_api_path = '/repos/ecomclub/';
-  $contents = github_api($github_api_path . $repo . '/contents' . $repo_path);
-  if ($contents) {
-    if (!is_array($contents)) {
-      // problem with API response
-      var_dump($contents);
-      exit();
-    }
-
-    for ($i = 0; $i < count($contents); $i++) {
-      switch ($contents[$i]->type) {
-        case 'dir':
-          // recursive
-          $deep_files = get_repo_docs($repo, '/' . $contents[$i]->path);
-          for ($ii = 0; $ii < count($deep_files); $ii++) {
-            $files[] = $deep_files[$ii];
-          }
-          break;
-
-        case 'file':
-          // save only Markdown files
-          if (substr($contents[$i]->name, -3) === '.md') {
-            $content = github_api($contents[$i]->url, true);
-            if ($content) {
-              // decode file content
-              $files[] = array(
-                'path' => $content->path,
-                'markdown' => base64_decode($content->content)
-              );
-            }
-          }
-          break;
+  for ($i = 0; $i < count($filenames); $i++) {
+    $filename = $filenames[$i];
+    if ($filename[0] !== '.') {
+      // check if it is a directory
+      $full_path = $dir . '/' . $filename;
+      if (is_dir($full_path)) {
+        // recursion
+        $deep_files = find_md_files($repo_dir, $path . $filename);
+        for ($ii = 0; $ii < count($deep_files); $ii++) {
+          $files[] = $deep_files[$ii];
+        }
+      } else if (is_file($full_path) && substr($filename, -3) === '.md') {
+        // valid Markdown file
+        // add to list
+        $files[] = array(
+          'path' => $path . $filename,
+          'markdown' => file_get_contents($full_path)
+        );
       }
     }
   }
@@ -62,7 +42,7 @@ $repos = array(
     'description' => 'Template specifications for E-Com Plus ecommerce themes'
   )
 );
-$docs_dir = __DIR__ . '/../../src/assets/json/els-developers/docs/';
+$docs_dir = __DIR__ . '/../../src/submodules/';
 // parse markdown to HTML
 // https://github.com/erusev/parsedown
 $parsedown = new Parsedown();
@@ -71,20 +51,9 @@ foreach ($repos as $repo => $page) {
   // array of Markdown files from current repository
   $files = null;
   $repo_dir = $docs_dir . $repo;
-  if (is_dir($repo_dir) && @$argv[1] !== 'update-' . $repo) {
-    // try to set $files object from JSON files content
-    $filenames = scandir($repo_dir);
-    $files = [];
-    for ($i = 0; $i < count($filenames); $i++) {
-      if (substr($filenames[$i], -5) === '.json') {
-        // parse JSON to associative array
-        $files[] = json_decode(file_get_contents($repo_dir . '/' . $filenames[$i]), true);
-      }
-    }
-  }
-  if (!$files || count($files) === 0) {
-    // request to GitHub API
-    $files = get_repo_docs($repo);
+  if (is_dir($repo_dir)) {
+    // try to set $files object from submodules .md files content
+    $files = find_md_files($repo_dir);
   }
 
   for ($i = 0; $i < count($files); $i++) {
